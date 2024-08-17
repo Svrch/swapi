fbee1384757e3b84d326d9340415001b5a7b7f65
Svrch <sapeshin@yandex.ru>
2024-08-17 18:49:37 +0500

    Commit

--- sw-test/src/modules/CharactersHeight/components/CharactersColumnChart.vue
+++ sw-test/src/modules/CharactersHeight/components/CharactersColumnChart.vue
@@ -26,7 +26,7 @@ const setChartData = () => ({
       label: 'Characters Height',
       data: charactersColumnChart.charactersHeight,
       backgroundColor: ['rgba(249, 115, 22, 0.2)', 'rgba(6, 182, 212, 0.2)', 'rgb(107, 114, 128, 0.2)', 'rgba(139, 92, 246 0.2)'],
-      borderColor: ['rgb(249, 115, 22)', 'rgb(6, 182, 212)', 'rgb(107, 114, 128)', 'rgb(139, 92, 246)'],
+      borderColor: ['rgb(249, 114, 22)', 'rgb(6, 182, 212)', 'rgb(107, 114, 128)', 'rgb(139, 92, 246)'],
       borderWidth: 1,
     },
   ],
